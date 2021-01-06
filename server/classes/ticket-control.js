1fccc8c888e4577fd3e9304abaae69e5fef5eae9
Rene <renealejandrosanchezmorales@gmail.com>
2021-01-06 11:56:15 -0300

    se optimiza el uso de guardado de informacion, se crea el reiniciar conteo y el siguiente para los tickets, se termina la logica de la comparacion de los dias de hoy

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -9,14 +9,27 @@ class TicketControl {
         let { ultimo, hoy } = require('../data/data.json');
 
         if (hoy === this.hoy) {
-
+            this.ultimo = ultimo;
         } else {
             this.reiniciarConteo();
         }
     }
 
     reiniciarConteo() {
+        this.ultimo = 0;
 
+        grabarInformacion();
+
+        return `Ticket ${this.ultimo}`;
+    }
+
+    siguiente() {
+        this.ultimo += 1;
+
+        grabarInformacion();
+    }
+
+    grabarInformacion() {
         let jsonData = {
             ultimo: this.ultimo,
             hoy: this.hoy
@@ -25,8 +38,6 @@ class TicketControl {
         let jsonDataString = JSON.stringify(jsonData);
 
         fs.writeFileSync("./server/data/data.json", jsonDataString);
-
-        console.log("Se ha inicializado el sistema")
     }
 
 }
